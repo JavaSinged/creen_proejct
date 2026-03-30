1027aa9c8f80aa922de4381bcf4c6dd973c980e9
kimnayoung7777 <k9075k@gmail.com>
2026-03-30 14:52:13 +0900

    member패키지, 클래스 파일생성 및 member-mapper파일명 오타 수정

---
+++ creen_carry_back/src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -0,0 +1,18 @@
+package kr.co.iei.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.co.iei.member.model.service.MemberService;
+
+@CrossOrigin(value="*")
+@RestController
+@RequestMapping(value="/members")
+public class MemberController {
+	
+	@Autowired
+	private MemberService memberService;
+
+}
---
+++ creen_carry_back/src/main/java/kr/co/iei/member/model/vo/Member.java
@@ -0,0 +1,15 @@
+package kr.co.iei.member.model.vo;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Alias(value="member")
+public class Member {
+
+}
