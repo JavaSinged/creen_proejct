d02d21c35690e960723d59f6f5fae3dace3357e8
JavaSinged <shinjiwoong1656@gmail.com>
2026-03-31 11:23:17 +0900

    signup-design

--- creen_proejct_front/src/pages/signup/Signup.jsx
+++ creen_proejct_front/src/pages/signup/Signup.jsx
@@ -1,14 +1,16 @@
-import styles from "./Signup.module.css";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import styles from './Signup.module.css';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
   const navigate = useNavigate();
+
   return (
     <div className={styles.signup_container}>
-      {/* 화면 최상단 로고 */}
+      {/* 화면 최상단 로고 - 클릭 시 홈으로 */}
       <header className={styles.header}>
-        <h1 className={styles.logo}>GreenCarry</h1>
+        <h1 className={styles.logo} onClick={() => navigate('/')}>
+          GreenCarry
+        </h1>
       </header>
 
       {/* 중앙 카드 세션 */}
@@ -18,19 +20,14 @@ const Signup = () => {
         <div className={styles.button_wrap}>
           <button
             className={styles.reg_button}
-            onClick={() => {
-              // 수정: /signup/을 빼고 App.js 경로와 맞춤
-              navigate("/userSignup");
-            }}
+            onClick={() => navigate('/userSignup')}
           >
             개인 회원가입
           </button>
+
           <button
             className={styles.reg_button}
-            onClick={() => {
-              // 수정: /signup/을 빼고 App.js 경로와 맞춤
-              navigate("/managerSignup");
-            }}
+            onClick={() => navigate('/managerSignup')}
           >
             사업자 회원가입
           </button>
@@ -38,7 +35,12 @@ const Signup = () => {
 
         <p className={styles.footer_text}>
           회원 정보를 잊어버리셨나요?
-          <span className={styles.find_link}>아이디/비밀번호 찾기</span>
+          <span
+            className={styles.find_link}
+            onClick={() => navigate('/account')}
+          >
+            아이디/비밀번호 찾기
+          </span>
         </p>
       </main>
     </div>
