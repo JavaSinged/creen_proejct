fdb523ad4283dbc3b9fdff0e74f33b6988b56059
choiseunghwan34 <chltmdghks7894@gmail.com>
2026-04-03 13:46:11 +0900

    관리자페이지 탭변경 슬라이드

--- creen_proejct_front/src/pages/mypage/admin/AdminDashboard.jsx
+++ creen_proejct_front/src/pages/mypage/admin/AdminDashboard.jsx
@@ -12,14 +12,19 @@ export default function AdminDashboard() {
     <div className={styles.dashboardContainer}>
       {/* 이미지와 동일한 버튼 그룹 (탭 메뉴) */}
       <div className={styles.tabContainer}>
+        {/* 뒤에서 움직이는 배경 바 */}
+        <div
+          className={`${styles.slideIndicator} ${activeTab === "inquiry" ? styles.right : styles.left}`}
+        ></div>
+
         <button
-          className={`${styles.tabButton} ${activeTab === "dashboard" ? styles.activeTab : ""}`}
+          className={`${styles.tabButton} ${activeTab === "dashboard" ? styles.activeText : ""}`}
           onClick={() => setActiveTab("dashboard")}
         >
           대시보드
         </button>
         <button
-          className={`${styles.tabButton} ${activeTab === "inquiry" ? styles.activeTab : ""}`}
+          className={`${styles.tabButton} ${activeTab === "inquiry" ? styles.activeText : ""}`}
           onClick={() => setActiveTab("inquiry")}
         >
           회원 문의 내역
