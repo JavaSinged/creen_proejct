a291392fce15b2e91bf55475e661d306f3a853d9
xxiyeon <tiaqhfl@nate.com>
2026-04-06 17:01:01 +0900

    Today-Grade-content

--- creen_carry_back/src/main/java/kr/co/iei/member/model/service/MemberService.java
+++ creen_carry_back/src/main/java/kr/co/iei/member/model/service/MemberService.java
@@ -220,5 +220,9 @@ public class MemberService {
         return memberDao.updateAddress(member);
     }
 
+	public int checkActiveOrder(String memberId) {
+		return memberDao.checkActiveOrder(memberId);
+	}
+
 
 }
